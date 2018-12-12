a1774eca9dfa8c42c83c4d7be646aa884aa0d75f
John Wen <John.Wen@ehealth.com>
2018-12-12 11:22:23 +0800

    add cart js

--- src/store/cart/index.js
+++ src/store/cart/index.js
@@ -1,7 +1,49 @@
-const state = {}
-const mutations = {}
-const actions = {}
-const getters = {}
+import axios from "axios";
+import { stat } from "fs";
+
+const state = {
+    cartItems: []
+}
+const mutations = {
+    UPDATE_CART_ITEMS(state,payload) {
+        state.cartItems = payload;
+    }
+}
+const actions = {
+    getCartItems({ commit }){
+       axios.get('/api/cartLists').then((res) => {
+           commit('UPDATE_CART_ITEMS',res.data);
+       })
+    },
+    addCartItem({ commit },cartItem){
+        axios.post('/api/cart/add',cartItem).then( res => {
+            commit("UPDATE_CART_ITEMS",res.data);
+        })
+    },
+    deleteCartItem({ commit }, cartItem){
+        axios.post('/api/cart/delete', cartItem).then( res => {
+            commit('UPDATE_CART_ITEMS', res.data);
+        })
+    },
+    deleteCartAll({ commit }){
+        axios.post('/api/cart/delete/all').then( res => {
+            commit('UPDATE_CART_ITEMS',res.data);
+        })
+    }
+}
+const getters = {
+    cartItems: state => state.cartItems,
+    cartTotal: state => {
+        return state.cartItems.reduce((acc,cartItem) => {
+            return (cartItem.price * cartItem.quantity) + acc;
+        },0).toFixed(2)
+    },
+    cartQuantity: state => {
+        return state.cartItems.reduce((acc,cartItem) => {
+            return cartItem.quantity + acc ;
+        },0)
+    }
+}
 
 const cartModule = {
     state,
